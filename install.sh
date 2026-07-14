c1ca9c6fa777d7ae09042da6fcc0087eb521b41c
ik0r <2631733+ik0r@users.noreply.github.com>
2026-07-14 11:22:35 +0800

    refactor: switch homebrew mirror from tuna to ustc
    Drop deprecated HOMEBREW_CORE_GIT_REMOTE (brew 4.0+ uses the JSON API)
    and keep only HOMEBREW_BREW_GIT_REMOTE for the install step.

--- install.sh
+++ install.sh
@@ -388,11 +388,7 @@ function install_homebrew(){
 
   must_program_exists "curl"
 
-  export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
-  export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
-  export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
-  export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
-  export HOMEBREW_PIP_INDEX_URL="https://pypi.tuna.tsinghua.edu.cn/simple"
+  export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
 
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/refs/heads/main/install.sh)"
 
